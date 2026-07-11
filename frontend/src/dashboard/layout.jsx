db06d7b50b7ae4ff505dcb6aa0c8d138340cc975
Sakshi Chavan <sakshichavandvg@gmail.com>
2026-07-11 10:27:29 +0530

    Delete frontend directory

--- frontend/src/dashboard/layout.jsx
+++
@@ -1,9 +0,0 @@
-import DashboardShell from "@/components/layout/DashboardShell";
-
-export default function DashboardLayout({ children }) {
-  return (
-    <DashboardShell>
-      {children}
-    </DashboardShell>
-  );
-}
